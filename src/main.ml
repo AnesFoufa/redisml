4a046009e4b96ee4a09e2b438f6249fb8d5ea4aa
Anès Foufa <anesfoufa@proton.me>
2024-12-30 13:14:35 +0100

    handle multiple PINGs

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,14 @@
 open Unix
 
+let rec handle_client client_fd =
+  let out_chan = out_channel_of_descr client_fd in
+  try
+    let _ = input_line (in_channel_of_descr client_fd) in
+    Printf.fprintf out_chan "+PONG\r\n";
+    flush out_chan;
+    handle_client client_fd
+  with End_of_file -> ()
+
 let () =
   let server_socket = socket PF_INET SOCK_STREAM 0 in
   setsockopt server_socket SO_REUSEADDR true;
@@ -7,8 +16,6 @@ let () =
   listen server_socket 1;
 
   let client_socket, _ = accept server_socket in
-  let response_buff = Bytes.of_string "+PONG\r\n" in
-  let len = Bytes.length response_buff in
-  let _ = write client_socket response_buff 0 len in
+  handle_client client_socket;
   close client_socket;
   close server_socket

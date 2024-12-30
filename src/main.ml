15e006d51e073cfee25515d31e9889cad142b0c0
Anès Foufa <anesfoufa@proton.me>
2024-12-30 12:56:54 +0100

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,11 @@
 open Unix
 
 let () =
-  (* Create a TCP server socket *)
   let server_socket = socket PF_INET SOCK_STREAM 0 in
   setsockopt server_socket SO_REUSEADDR true;
   bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
   listen server_socket 1;
 
-  (* Uncomment this block to pass the first stage *)
   let client_socket, _ = accept server_socket in
   let response_buff = Bytes.of_string "+PONG\r\n" in
   let len = Bytes.length response_buff in

0e712392d21b48f977c0be909977af21e77c79b8
Anès Foufa <anesfoufa@proton.me>
2025-01-09 12:26:12 +0100

    multithreades server

--- src/main.ml
+++ src/main.ml
@@ -9,13 +9,21 @@ let rec handle_client client_fd =
     handle_client client_fd
   with End_of_file -> ()
 
+let rec accept_socket_and_handle_client pool server_socket =
+  let client_socket, _ = accept server_socket in
+  let work () =
+    print_endline "New client";
+    handle_client client_socket;
+    close client_socket;
+    print_endline "Client left"
+  in
+  let _ = Thread_pool.add_work pool work in
+  accept_socket_and_handle_client pool server_socket
+
 let () =
   let server_socket = socket PF_INET SOCK_STREAM 0 in
   setsockopt server_socket SO_REUSEADDR true;
   bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
   listen server_socket 1;
-
-  let client_socket, _ = accept server_socket in
-  handle_client client_socket;
-  close client_socket;
-  close server_socket
+  let pool = Thread_pool.create ~max_num_threads:4 () |> Core.ok_exn in
+  accept_socket_and_handle_client pool server_socket

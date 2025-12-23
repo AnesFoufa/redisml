ac1d892ac8bb19eea842cb1c845d1bb882ca7096
Anès Foufa <anesfoufa@proton.me>
2025-12-23 22:02:53 +0000

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -26,6 +26,9 @@ let handle_connection (ic, oc) =
           (* Add to buffer *)
           buffer := !buffer ^ data;
 
+          (* Empty RDB file (hex encoded) - minimal valid RDB *)
+          let empty_rdb = "\x52\x45\x44\x49\x53\x30\x30\x31\x31\xfa\x09\x72\x65\x64\x69\x73\x2d\x76\x65\x72\x05\x37\x2e\x32\x2e\x30\xfa\x0a\x72\x65\x64\x69\x73\x2d\x62\x69\x74\x73\xc0\x40\xfa\x05\x63\x74\x69\x6d\x65\xc2\x6d\x08\xbc\x65\xfa\x08\x75\x73\x65\x64\x2d\x6d\x65\x6d\xc2\xb0\xc4\x10\x00\xfa\x08\x61\x6f\x66\x2d\x62\x61\x73\x65\xc0\x00\xff\xf0\x6e\x3b\xfe\xc0\xff\x5a\xa2" in
+
           (* Try to parse and process commands from buffer *)
           let rec process_buffer () =
             match Resp.parse !buffer with
@@ -36,6 +39,17 @@ let handle_connection (ic, oc) =
                 let response_str = Resp.serialize response in
                 let* () = Lwt_io.write oc response_str in
                 let* () = Lwt_io.flush oc in
+
+                (* Check if this was a PSYNC command - need to send RDB file after *)
+                let* () =
+                  match Command.parse cmd with
+                  | Some (Command.Psync _) ->
+                      (* Send empty RDB file as bulk string *)
+                      let rdb_response = Printf.sprintf "$%d\r\n%s" (String.length empty_rdb) empty_rdb in
+                      let* () = Lwt_io.write oc rdb_response in
+                      Lwt_io.flush oc
+                  | _ -> Lwt.return_unit
+                in
                 process_buffer ()
             | None ->
                 (* Incomplete command, wait for more data *)

fd421acc444a0819e3677d10164b0e52aa70e0fc
Anès Foufa <anesfoufa@proton.me>
2025-12-23 11:56:51 +0000

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -1,16 +1,36 @@
 open Unix
 
-let () =
-  (* You can use print statements as follows for debugging, they'll be visible when running tests. *)
-  Printf.eprintf "Logs from your program will appear here!\n";
+let handle_connection client_socket =
+  Printf.eprintf "Client connected\n%!";
+  close client_socket
 
-  (* Create a TCP server socket *)
+let start_server port =
+  Printf.eprintf "Starting Redis server on port %d\n%!" port;
   let server_socket = socket PF_INET SOCK_STREAM 0 in
   setsockopt server_socket SO_REUSEADDR true;
-  bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
-  listen server_socket 1;
+  bind server_socket (ADDR_INET (inet_addr_any, port));
+  listen server_socket 5;
 
-  (* Uncomment the code below to pass the first stage *)
-  (* let (client_socket, _) = accept server_socket in *)
-  (* close client_socket; *)
-  (* close server_socket *)
+  (* Accept connections in a loop *)
+  while true do
+    let client_socket, _client_addr = accept server_socket in
+    (* Handle each connection in a separate thread *)
+    let _thread_id =
+      Thread.create
+        (fun () ->
+          handle_connection client_socket)
+        ()
+    in
+    ()
+  done
+
+let () =
+  try
+    start_server 6379
+  with
+  | Unix_error (err, fn, arg) ->
+      Printf.eprintf "Unix error: %s(%s): %s\n%!" fn arg (error_message err);
+      exit 1
+  | e ->
+      Printf.eprintf "Error: %s\n%!" (Printexc.to_string e);
+      exit 1
